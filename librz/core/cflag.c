59bbf1dce6f1d3c22da0c81ec0b29938f134c26e
Anton Kochkov <anton.kochkov@gmail.com>
2023-07-21 14:14:33 +0800

    Show realname in `fl.t` table output

--- librz/core/cflag.c
+++ librz/core/cflag.c
@@ -107,7 +107,8 @@ static bool print_flag_table(RzFlagItem *flag, void *user) {
 	}
 	if (!RZ_STR_ISEMPTY(flag->name)) {
 		const char *spaceName = (flag->space && flag->space->name) ? flag->space->name : "";
-		rz_table_add_rowf(u->tbl, "Xdss", flag->offset, flag->size, spaceName, flag->name);
+		const char *realname = RZ_STR_ISEMPTY(flag->realname) ? flag->name : flag->realname;
+		rz_table_add_rowf(u->tbl, "Xdsss", flag->offset, flag->size, spaceName, flag->name, realname);
 	}
 	return true;
 }
@@ -140,7 +141,7 @@ static void flag_print(RzFlag *f, RzCmdStateOutput *state, ut64 range_from, ut64
 		break;
 	case RZ_OUTPUT_MODE_TABLE:
 		u.tbl = state->d.t;
-		rz_cmd_state_output_set_columnsf(state, "Xdss", "addr", "size", "space", "name");
+		rz_cmd_state_output_set_columnsf(state, "Xdsss", "addr", "size", "space", "name", "realname");
 		rz_flag_foreach_space(f, rz_flag_space_cur(f), print_flag_table, &u);
 		break;
 	default:
